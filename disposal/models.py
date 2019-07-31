0828ca4f9f01346d55706f91e798bb2ceaeb7632
JIEuNo <wisdom990629@naver.com>
2019-07-31 19:43:22 +0900

    first commit

---
+++ disposal/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Disposal(models.Model):
+    title=models.CharField(max_length=200)
+    pub_date=models.DateTimeField('date published')
+    address=models.CharField(max_length=200)
+    name=models.CharField(max_length=200)
+    item=models.CharField(max_length=200)
+    phone=models.CharField(max_length=200)
+    etc=models.TextField()
+    photo = models.ImageField(default='media/default_image.png')
+
+    def __str__(self):
+        return self.title
